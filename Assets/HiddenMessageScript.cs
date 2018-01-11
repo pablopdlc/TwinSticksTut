7267682f70e7d2f331b8c22e5618c3b4ef347da3
PabsLaptop <pablopdlc@yahoo.com>
2018-01-10 22:31:17 -0800

    Testing first commit test script and test Scene

---
+++ Assets/HiddenMessageScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenMessageScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hidden Message!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

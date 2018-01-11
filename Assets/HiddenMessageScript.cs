1c4416325edb4070813cfb613e9e9620662596d4
PabsLaptop <pablopdlc@yahoo.com>
2018-01-10 22:32:53 -0800

    New line to hidden message script

--- Assets/HiddenMessageScript.cs
+++ Assets/HiddenMessageScript.cs
@@ -7,6 +7,7 @@ public class HiddenMessageScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hidden Message!");
+        Debug.Log("Watching AGDQ2018");
 	}
 	
 	// Update is called once per frame
